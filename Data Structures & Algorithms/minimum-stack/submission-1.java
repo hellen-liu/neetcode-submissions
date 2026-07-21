0a1969030d3e30c4011c579f74525001a3716a91
hellen-liu <hellen_yuyanliu@hotmail.com>
2026-07-21 17:45:13 +0800

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,37 @@
+class MinStack {
+//keep two stacks, one built specifically for recording the current mininum element
+//the normal stack would do all the push, pop and top.
+    
+    private Stack<Integer>stk;
+    private Stack<Integer>min;
+
+    public MinStack() {
+        stk = new Stack<>();
+        min = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stk.push(val);
+        try{
+            min.push((min.peek()<val)?min.peek():val);
+        }catch (EmptyStackException e){
+            min.push(val);
+        }
+    }
+    
+    public void pop() {
+        stk.pop();
+        min.pop();
+        
+    }
+    
+    public int top() {
+        return stk.peek();
+        
+    }
+    
+    public int getMin() {
+        return min.peek();
+        
+    }
+}
